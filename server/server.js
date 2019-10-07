4d238f2d2035988eead25354d23909b2c15697c8
Talent Malaba <malabatalent@gmail.com>
2019-10-07 10:42:03 +0800

    configured stuff for Heroku deployment

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const port = process.env.PORT || 5000;
 
 // bootstrap database connection
@@ -21,6 +21,12 @@ app.use('/api/contacts', contacts);
 app.use('/api/users', users);
 app.use('/api/auth', auth);
 
+// serve the React client assets for production
+if(process.env.NODE_ENV === 'production') {
+    // set the static dir
+    app.use(express.static('../client/build'));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html')));
+}
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
 
